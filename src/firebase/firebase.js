1c6cd4a0219df37b34b25938f16faa1b5136e21d
woodley <gelinw3@gmail.com>
2023-09-30 21:16:35 -0400

    fix: fix undefined env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,14 +6,15 @@ import { useDispatch } from "react-redux";
 import { getStorage } from "firebase/storage";
 //web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.WEB_API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.API_ID ,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_WEB_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_API_ID ,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
